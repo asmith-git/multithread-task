e0c1780bec0a70f53d08b68ec4c31765da5c64cd
SolaireLibrary <solairelibrary@mail.com>
2017-01-19 11:44:32 +0000

    Added headers for thread_pool

---
+++ include/as/multithread_task/thread_pool.hpp
@@ -0,0 +1,32 @@
+#ifndef ASMITH_THEAD_POOL_HPP
+#define ASMITH_THEAD_POOL_HPP
+
+// Copyright 2017 Adam Smith
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "task_dispatcher.hpp"
+
+namespace as {
+	class thread_pool : public task_dispatcher {
+	protected:
+		// Inherited from task_dispatcher
+		void schedule_task(task_ptr) override;
+	public:
+		thread_pool();
+		thread_pool(size_t);
+		~thread_pool();
+	};
+}
+
+#endif
---
+++ src/as/multithread_task/thread_pool.cpp
@@ -0,0 +1,36 @@
+// Copyright 2017 Adam Smith
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "as/multithread_task/thread_pool.hpp"
+
+namespace as {
+	// thread_pool
+
+	thread_pool::thread_pool() {
+		//!\todo Implement
+	}
+
+	thread_pool::thread_pool(size_t aThreads) {
+		//!\todo Implement
+	}
+
+	thread_pool::~thread_pool() {
+		//!\todo Implement
+	}
+
+	void thread_pool::schedule_task(task_ptr aTask) {
+		//!\todo Implement
+	}
+
+}
