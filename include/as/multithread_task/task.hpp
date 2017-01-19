f879f03e4ca37958770f90f37e92308cddba983e
SolaireLibrary <solairelibrary@mail.com>
2017-01-19 11:29:02 +0000

    Added task inplementaton

---
+++ include/as/multithread_task/task.hpp
@@ -0,0 +1,72 @@
+#ifndef ASMITH_TASK_HPP
+#define ASMITH_TASK_HPP
+
+// Copyright 2017 Adam Smith
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <future>
+#include "task_interface.hpp"
+
+namespace as {
+	template<class T>
+	class task : public task_interface {
+	private:
+		std::promise<T> mPromise;
+	protected:
+		virtual T execute_for_return(task_controller&) = 0;
+	public:
+		virtual ~task() {}
+
+		// Inherited from task_interface
+
+		void execute(task_controller& aController) override {
+			try{
+				mPromise.set_value(execute_for_return(aController));
+			}catch(std::exception& e) {
+				mPromise.set_exception(std::current_exception());
+			}
+		}
+
+		void* get_promise() override {
+			return &mPromise;
+		}
+	};
+
+	template<>
+	class task<void> : public task_interface {
+	private:
+		std::promise<void> mPromise;
+	protected:
+		virtual void execute_for_return(task_controller&) = 0;
+	public:
+		virtual ~task() {}
+
+		// Inherited from task_interface
+
+		void execute(task_controller& aController) override {
+			try{
+				execute_for_return(aController);
+				mPromise.set_value();
+			}catch (std::exception& e) {
+				mPromise.set_exception(std::current_exception());
+			}
+		}
+
+		void* get_promise() override {
+			return &mPromise;
+		}
+	};
+}
+
+#endif
